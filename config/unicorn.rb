05dfe18e19eba881e3bcdf5f88cd1b8836b787ea
羅中海 <luozhonghai@163.com>
2016-01-12 23:52:56 +0800

    set up deploy file

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/www/web/hashvr_fruit_server/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.hashvr_fruit_server.sock"
+worker_processes 2
+timeout 30
